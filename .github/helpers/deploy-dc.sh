8d716d40017e6b6e1435851fb0c6e81538047f44
Adam Kroon <Adam.Kroon@gov.bc.ca>
2022-05-06 09:03:07 -0700

    Update deploy-dc.sh

--- .github/helpers/deploy-dc.sh
+++ .github/helpers/deploy-dc.sh
@@ -2,7 +2,7 @@
 
 # script to process slam-api deployment config template and deploy it to a given namespace
 
-oc process -f /home/runner/work/citz-imb-slam-app/citz-imb-slam-app/openshift/templates/citz-imb-slam-app/dc.yaml --namespace=$NAMESPACE \
+oc process -f /home/runner/work/citz-imb-slam-app/citz-imb-slam-app/openshift/templates/citz-imb-slam-app/$FILE_NAME --namespace=$NAMESPACE \
     -p APPLICATION_NAME=$APPLICATION_NAME \
     -p LICENSE_PLATE=$LICENSE_PLATE \
     -p ENVIRONMENT=$ENVIRONMENT | \
